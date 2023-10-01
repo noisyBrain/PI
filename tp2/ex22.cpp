61f7404b2bcd708eff18c3bc76c76b9126035349
Tomás <tomasarreguib@gmail.com>
2023-10-01 17:04:02 -0300

    chore(ex22): basic scaffold

---
+++ tp2/ex22.cpp
@@ -0,0 +1,24 @@
+/*
+
+  Realizar un programa que almacene en un arreglo la lista de invitados a una fiesta.
+  El salón donde se realizará tiene capacidad máxima para 150 personas.
+  De cada invitado se conoce nombre, apellido, sexo y edad.
+  Se ingresarán datos hasta que el nombre sea igual a “ZZZ” o hasta que la capacidad del salón esté completa.
+  Una vez finalizada la carga:
+
+  a) Informar la cantidad de invitados que asistirán a la fiesta.
+  b) Realizar una función que liste los nombres de las mujeres cuya edad sea <= 20.
+  c) Realizar una función que, dado un apellido y nombre, permita eliminar un invitado en caso de no poder
+  asistir.
+
+*/
+
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main () {
+  
+  return 0;
+}
