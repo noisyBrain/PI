d7c6ddfe0c2d3a1a799b17877aefd67b01e05330
Tomás <tomasarreguib@gmail.com>
2023-10-05 18:31:35 -0300

    chore(ex25): basic scaffold

---
+++ tp2/ex25.cpp
@@ -0,0 +1,25 @@
+/*
+
+  Implementar un sistema que permita administrar datos de clientes de un comercio para enviarles promociones.
+  De cada cliente se tiene: nombre y apellido, DNI, e-mail, fecha de cumpleaños en formato DDMM, monto total
+  de compras en el último año.
+
+  a) Realizar el módulo de carga. La misma finaliza con el nombre "z". Los clientes deben almacenarse
+  ordenados por número de DNI.
+
+  b) Informar cuántos clientes cumplen años en cada uno de los 12 meses del año. Utilizar un arreglo
+  auxiliar para contabilizarlos.
+
+  c) Informar el nombre del cliente con el mayor monto de compras.
+
+*/
+
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main () {
+  
+  return 0;
+}
