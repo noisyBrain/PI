cbf37525a9bd58c65f6ac04965f977a5fddbb1b7
Tomás <tomasarreguib@gmail.com>
2023-10-05 11:29:00 -0300

    chore(ex23): basic scaffold

---
+++ tp2/ex23.cpp
@@ -0,0 +1,20 @@
+/*
+
+  La universidad posee información histórica sobre los estudiantes (no más de 1000) de una materia determinada. 
+  De cada uno almacena: nombre y apellido, legajo, cantidad de inasistencias a clase, calificación obtenida. 
+  Almacenar esta información en un arreglo de structs. Se pide:
+
+  a) Imprimir nombre y apellido de los alumnos que tuvieron más de 5 inasistencias.
+  b) Imprimir número de legajo de los alumnos cuya calificación promedio sea mayor o igual a la calificación
+      promedio total (requiere calcular un promedio de las calificaciones de todos los alumnos en el listado).
+  c) Imprimir el número de legajo de aquellos alumnos que tienen promedio mayor o igual a nueve.
+  d) Dado el legajo de un alumno, eliminarlo del listado.
+
+Nota: analizar si es posible reutilizar código en más de uno de los incisos.
+
+*/
+
+int main () {
+  
+  return 0;
+}
