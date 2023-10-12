0d9051fe0c543f28a085bc9cb0cbd5cdc2d9158d
Tomás <tomasarreguib@gmail.com>
2023-10-11 23:05:10 -0300

    chore(ex5): basic scaffolding for ex5 second part (insert at the end of the linked list)

---
+++ tp3/ex5_b.cpp
@@ -0,0 +1,25 @@
+/*
+
+  Se lee de teclado una secuencia de números enteros, finalizando cuando el usuario ingresa un cero (que no
+  debe insertarse). Los números leídos deberán ser almacenados en una lista enlazada simple. Hacer tres
+  versiones, insertando en cada una con diferente criterio, a saber:
+  b) Al final de la lista.
+
+  Finalmente, imprimir la lista.
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+struct Node {
+  int data;
+  Node *next;
+};
+
+int main() {
+
+  return 0;
+}
+
