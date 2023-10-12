ff85aebe6ca23a5c8d3c0700eef61549a6aefa56
Tomás <tomasarreguib@gmail.com>
2023-10-11 23:12:15 -0300

    chore(ex5_c): basic scaffolding for exercise 5, third point

---
+++ tp3/ex_c.cpp
@@ -0,0 +1,20 @@
+/* 
+
+  Se lee de teclado una secuencia de números enteros, finalizando cuando el usuario ingresa un cero (que no
+  debe insertarse). Los números leídos deberán ser almacenados en una lista enlazada simple. Hacer tres
+  versiones, insertando en cada una con diferente criterio, a saber:
+  c) En la posición correcta para que la lista quede ordenada de menor a mayor. Si el número a insertar ya
+     se encontraba en la lista, deberá quedar antes que todas sus repeticiones.
+
+  Finalmente, imprimir la lista.
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main () {
+
+  return 0;
+}
