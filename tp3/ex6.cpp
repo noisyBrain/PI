e17eb4b13183e185cb4f2ea5b06c25ff8cb36814
Tomás <tomasarreguib@gmail.com>
2023-10-12 22:42:13 -0300

    chore(ex6): added missing second parte of the exercise

--- tp3/ex6.cpp
+++ tp3/ex6.cpp
@@ -3,6 +3,9 @@
   Permitir al usuario guardar en una lista enlazada simple los nombres de los alumnos de un curso,
   insertando al final de la lista y cortando la carga cuando se inserte como nombre "x",
   ya sea en mayúscula o en minúscula (el  cual no debe insertarse en la lista).
+  A continuación, solicitar al usuario el nombre de un alumno e informar si se encuentra en la lista o no.
+  Por último, eliminar a la alumna "Josefina Ortega" de la lista (se asume que no se han almacenado nombres
+  repetidos).
 
 */
 
