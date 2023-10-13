17fb32e0b368cf5a21e3f2c6391301d24ea00ef6
Tomás <tomasarreguib@gmail.com>
2023-10-12 22:05:51 -0300

    chore(ex6): basic scaffold

---
+++ tp3/ex6.cpp
@@ -0,0 +1,16 @@
+/*
+
+  Permitir al usuario guardar en una lista enlazada simple los nombres de los alumnos de un curso,
+  insertando al final de la lista y cortando la carga cuando se inserte como nombre "x",
+  ya sea en mayúscula o en minúscula (el  cual no debe insertarse en la lista).
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main () {
+
+  return 0;
+}
