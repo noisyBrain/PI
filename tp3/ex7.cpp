ded11824d4534efc5e6a389131c99f23cdcaf240
Tomás <tomasarreguib@gmail.com>
2023-10-14 19:23:56 -0300

    chore(ex7): basic scaffolding

---
+++ tp3/ex7.cpp
@@ -0,0 +1,22 @@
+/*
+
+  Se lee de teclado una secuencia de números que finaliza con la primera ocurrencia de un número negativo, el
+  cual no debe ser procesado. Los números deben ser almacenados en una lista enlazada simple. Se solicita:
+
+  A. Leer de teclado un número e informar si existe en la lista.
+  B. Leer de teclado un número e informar la cantidad de ocurrencias del número en la lista.
+  C. Implementar una función que reciba la lista de números y genere dos nuevas listas, una con los
+     números pares y otra con los impares. Las nuevas listas deberán estar formadas con los nodos de la lista
+     original, la que quedará vacía después de invocar a la función. Imprimir las listas resultantes.
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main () {
+  
+
+  return 0;
+}
