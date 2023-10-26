23d37b974ce37c9e938d320cdea8d790e9d817e3
Tomás <tomasarreguib@gmail.com>
2023-10-26 09:24:47 -0300

    feat(ex7): added new function and completed exercise

--- tp3/ex7.cpp
+++ tp3/ex7.cpp
@@ -3,11 +3,11 @@
   Se lee de teclado una secuencia de números que finaliza con la primera ocurrencia de un número negativo, el
   cual no debe ser procesado. Los números deben ser almacenados en una lista enlazada simple. Se solicita:
 
-  A. Leer de teclado un número e informar si existe en la lista.
-  B. Leer de teclado un número e informar la cantidad de ocurrencias del número en la lista.
+  A. Leer de teclado un número e informar si existe en la lista. ✅
+  B. Leer de teclado un número e informar la cantidad de ocurrencias del número en la lista. ✅
   C. Implementar una función que reciba la lista de números y genere dos nuevas listas, una con los
      números pares y otra con los impares. Las nuevas listas deberán estar formadas con los nodos de la lista
-     original, la que quedará vacía después de invocar a la función. Imprimir las listas resultantes.
+     original, la que quedará vacía después de invocar a la función. Imprimir las listas resultantes. ✅
 
 */
 
@@ -69,6 +69,40 @@ void countOcurrences(Node *head, int numberToSearch) {
   }
 
   cout << "La cantidad de ocurrencias del número " << numberToSearch << " es de: " << counter << endl;
+}
+
+Node* devideList(Node *head, Node *&evenHead, Node *&oddHead) {
+  if (head == nullptr) {
+    cout << "La lista está vacía!\n";
+    return head;
+  }
+
+  Node *aux;
+
+  while (head != nullptr) {
+    aux = head;
+    head = head->next;
+    aux->next = nullptr;
+
+    if (aux->data % 2 == 0) {
+      evenHead = insertNodeAtTheEnd(evenHead, aux);
+
+    } else {
+      oddHead = insertNodeAtTheEnd(oddHead, aux);
+    }
+  }
+
+  return head;
+}
+
+void printList(Node *head) {
+  cout << "\nLista final: ";
+
+  for (Node *aux = head; aux != nullptr; aux = aux->next) {
+    cout << aux->data << ", ";
+  }
+
+  cout << endl;
 }
 
 Node* takeInput(Node *head) {
