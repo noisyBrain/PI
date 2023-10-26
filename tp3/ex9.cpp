9e8d11470c2674f4585e04737ce7c8255ffe6c35
Tomás <tomasarreguib@gmail.com>
2023-10-26 16:05:39 -0300

    feat(ex9): added function to insert in descending order

--- tp3/ex9.cpp
+++ tp3/ex9.cpp
@@ -1,8 +1,8 @@
 /*
 
 Desarrollar un programa que realice las siguientes operaciones en listas enlazadas simples de números enteros.
-  A. Cargar una lista insertando los elementos ordenados en forma descendente. Finaliza luego de ingresar
-     el cero, que también se debe agregar a la lista.
+  A. Cargar una lista insertando los elementos ordenados en forma descendente. Finaliza luego de ingresar 
+     el cero, que también se debe agregar a la lista. ✅
   B. Eliminar las ocurrencias de los números que son múltiplos de 3.
   C. Imprimir la lista antes y después de la eliminación.
 
@@ -17,8 +17,48 @@ struct Node {
   Node *next;
 };
 
+Node* insertInDescendingOrder(Node *head, int number) {
+  Node *newNode = new Node();
+  newNode->data = number;
+
+  if (head == nullptr || head->data < number) {
+    newNode->next = head;
+    head = newNode;
+
+  } else {
+    Node *aux = head;
+
+    while (aux != nullptr && aux->next->data > number) {
+      aux = aux->next;
+    }
+
+    newNode->next = aux->next;
+    aux->next = newNode;
+  }
+
+  return head;
+}
+
+Node* takeUserPrompt(Node *head) {
+  int number = 0;
+
+  cout << "Ingrese un número entero (0 para salir): ";
+  cin >> number;
+
+  while (number != 0) {
+    head = insertInDescendingOrder(head, number);
+
+    cout << "Ingrese otro número entero (0 para salir): ";
+    cin >> number;
+  }
+
+  return head;
+}
+
 int main () {
-  
+  Node *head = nullptr;
+
+  head = takeUserPrompt(head);
 
   return 0;
 }
