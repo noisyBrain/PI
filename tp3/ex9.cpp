b44af451b92609a676a3ee9dc4f1a364db17d799
Tomás <tomasarreguib@gmail.com>
2023-10-26 16:39:41 -0300

    feat(ex9): added functions to delete ocurrences

--- tp3/ex9.cpp
+++ tp3/ex9.cpp
@@ -17,6 +17,37 @@ struct Node {
   Node *next;
 };
 
+Node *deleteOcurrences(Node *head, int number) {
+  if (head == nullptr) {
+    cout << "La lista está vacía!" << endl;
+    return head;
+  }
+
+  Node *toDelete;
+  Node *aux = head;
+
+  while (aux != nullptr) {
+    if (aux->data == number) {
+      toDelete = head;
+      head = head->next;
+      aux = head;
+      delete toDelete;
+
+    } else {
+      if (aux->next != nullptr && aux->next->data == number) {
+        toDelete = aux->next;
+        aux->next = toDelete->next;
+        delete toDelete;
+
+      } else {
+        aux = aux->next;
+      }
+    }
+  }
+
+  return head;
+}
+
 Node* insertInDescendingOrder(Node *head, int number) {
   Node *newNode = new Node();
   newNode->data = number;
@@ -55,10 +86,25 @@ Node* takeUserPrompt(Node *head) {
   return head;
 }
 
+Node* takePromptToDeleteOcurrences(Node *head) {
+  int number = 0;
+
+  cout << "Ingrese el número que desea elminar: ";
+  cin >> number;
+
+  head = deleteOcurrences(head, number);
+
+  return head;
+}
+
 int main () {
   Node *head = nullptr;
 
   head = takeUserPrompt(head);
+  cout << "\nDespués de agregar los elementos: \n\n";
+
+  head = takePromptToDeleteOcurrences(head);
+  cout << "\nDespués de eleminar las ocurrencias: \n\n";
 
   return 0;
 }
