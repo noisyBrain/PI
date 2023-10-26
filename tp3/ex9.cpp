05f0d817efd3d83e72ca1cf93175cd8d54b32e0a
Tomás <tomasarreguib@gmail.com>
2023-10-26 11:24:43 -0300

    chore(ex9)

---
+++ tp3/ex9.cpp
@@ -0,0 +1,19 @@
+/*
+
+Desarrollar un programa que realice las siguientes operaciones en listas enlazadas simples de números enteros.
+  A. Cargar una lista insertando los elementos ordenados en forma descendente. Finaliza luego de ingresar
+     el cero, que también se debe agregar a la lista.
+  B. Eliminar las ocurrencias de los números que son múltiplos de 3.
+  C. Imprimir la lista antes y después de la eliminación.
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main () {
+  
+
+  return 0;
+}
