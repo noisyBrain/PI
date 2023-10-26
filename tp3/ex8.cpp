907053d7881304fa0412f522857f0a01fcd7f4f0
Tomás <tomasarreguib@gmail.com>
2023-10-26 09:51:24 -0300

    chore(ex8): basic scaffolding

---
+++ tp3/ex8.cpp
@@ -0,0 +1,24 @@
+/*
+
+  Implementar una función que reciba dos parámetros: 
+    - una lista enlazada simple de números enteros 
+    - un número entero. 
+  La función debe eliminar todas las ocurrencias del número indicado en el segundo parámetro.
+  Retornar la cantidad de ocurrencias eliminadas. 
+
+  Nota: se debe pasar una sola vez por cada uno de los elementos de la lista.
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main () {
+  int number = 0;
+
+  cout << "Ingresá un número para eliminar todas las ocurrencias de la lista: ";
+  cin >> number;
+
+  return 0;
+}
