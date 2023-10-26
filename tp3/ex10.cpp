8a77b8ecd3781dec9f7d6565144a778911ab13f0
Tomás <tomasarreguib@gmail.com>
2023-10-26 16:59:51 -0300

    chore(ex10): basic scaffolding

---
+++ tp3/ex10.cpp
@@ -0,0 +1,17 @@
+/*
+
+Cargar una lista enlazada simple con palabras ingresadas por el usuario, las cuales deben almacenarse en
+minúsculas independientemente de cómo las ingrese. Luego, a partir de esa lista, generar una nueva lista con
+las palabras que se encuentran repetidas. Finalmente, imprimir ambas listas.
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main () {
+  
+
+  return 0;
+}
