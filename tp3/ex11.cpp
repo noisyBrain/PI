66256d12587ee0914ba7db22d15ebde99b0c73da
Tomás <tomasarreguib@gmail.com>
2023-11-05 18:54:26 -0300

    feat(ex11)

--- tp3/ex11.cpp
+++ tp3/ex11.cpp
@@ -6,11 +6,82 @@
 
 */
 
-#include <iostream>
+#include<iostream>
+#include<string>
 
 using namespace std;
 
+struct Node {
+  string data;
+  Node *next;
+};
+
+Node* insertAtTheEnd(Node *mergedListHead, Node *newNode) {
+  if (mergedListHead == nullptr) {
+    return newNode;
+  }
+
+  Node *aux = mergedListHead;
+  while (aux->next != nullptr) {
+    aux = aux->next;
+  }
+
+  aux->next = newNode;
+
+  return mergedListHead;
+}
+
+Node* gotoNextNode(Node *&listToMerge, Node *&newList) {
+  while (listToMerge != nullptr) {
+    Node *previousNode = listToMerge;
+
+    previousNode = listToMerge;
+    listToMerge = listToMerge->next;
+    newList = insertAtTheEnd(newList, previousNode);
+  }
+
+  return newList;
+}
+
+Node* merge(Node *list1, Node *list2) {
+  Node *mergedList = nullptr;
+
+  while (list1 != nullptr && list2 != nullptr) {
+    if (list1->data <= list2->data) {
+      mergedList = insertAtTheEnd(mergedList, list1);
+      list1 = list1->next;
+
+    } else {
+      mergedList = insertAtTheEnd(mergedList, list2);
+      list2 = list2->next;
+    }
+  }
+
+  // TODO: ask for this
+  mergedList = insertAtTheEnd(mergedList, list2);
+
+  return mergedList;
+}
+
+void printList(Node *head) {
+  for (Node *aux = head; aux != nullptr; aux = aux->next) {
+    cout << "aux->data: " << aux->data << "\n";
+  }
+
+  cout << endl;
+}
+
 int main () {
+  Node *firstList = new Node {"hola", nullptr};
+  Node *secondList = new Node {"chau", nullptr};
+  
+  firstList->next = new Node {"partido", nullptr};
+  secondList->next = new Node {"arbol", nullptr};
+
+  Node *mergedList = merge(firstList, secondList);
+
+  cout << "merged List... \n";
+  printList(mergedList);
 
   return 0;
 }
