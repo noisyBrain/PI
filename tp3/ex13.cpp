24ba459284ffbe993ab8062acd5e526538e22819
Tomás <tomasarreguib@gmail.com>
2023-11-05 23:10:40 -0300

    chore(ex13): basic scaffolding

---
+++ tp3/ex13.cpp
@@ -0,0 +1,20 @@
+/*
+
+  Dos sucursales de Fiestísima disponen cada una de una lista enlazada simple con el detalle de las ventas
+  realizadas. De cada venta se conoce: 
+    - código de producto 
+    - cantidad vendida (un producto pudo haber sido vendido 0, 1 o más veces en la misma sucursal). 
+  Las listas de ventas están ordenadas de menor a mayor por código de producto. 
+  Realizar una función que reciba las dos listas y retorne una nueva lista unificando todas las
+  ventas, ordenada por el mismo criterio. Utilizar un algoritmo destructivo (que reutilice los nodos de las listas
+  originales). Agregar las funciones auxiliares que sean necesarias.
+
+*/
+
+#include<iostream>
+#include<string>
+
+int main () {
+
+  return 0;
+}
