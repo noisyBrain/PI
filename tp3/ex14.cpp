8c1cc24d8a2350d3c7f02fc1060c43cc38457c24
Tomás <tomasarreguib@gmail.com>
2023-11-07 22:45:08 -0300

    chore(ex14): basic scaffolding

---
+++ tp3/ex14.cpp
@@ -0,0 +1,29 @@
+/*
+
+  Generar una lista con los datos personales de los alumnos de un colegio 
+   - legajo, 
+   - apellido y nombre
+   - DNI) 
+
+  y las notas de tres materias: 
+   - matemática,
+   - literatura 
+   - geografía (para cada alumno se cargarán las tres materias). 
+
+  Las notas pueden ir de 1 a 10 y se debe validar que el usuario no ingrese notas incorrectas,
+  volviendo a pedir una nueva nota cada vez que ingrese una incorrecta, hasta que ingrese un valor
+  en el rango indicado. Se pide informar la cantidad de alumnos que aprobaron las tres materias
+  (se aprueba con 7) y el porcentaje que representan los aprobados sobre el total de alumnos.
+
+- generar struct de alumno y struct de nodo
+- 
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main () {
+
+  return 0;
+}
