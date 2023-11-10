90d6b246604a978128a7270359a45fa5652deeae
Tomás <tomasarreguib@gmail.com>
2023-11-10 17:02:10 -0300

    chore(ex16): basic scaffolding

---
+++ tp3/ex16.cpp
@@ -0,0 +1,15 @@
+/*
+  Se lee de teclado una secuencia de números enteros, finalizando cuando el usuario ingresa un cero (que no
+  debe insertarse). Los números leídos deberán ser almacenados en una lista circular. Hacer dos versiones,
+  insertando en cada una con diferente criterio, a saber:
+    a) Al inicio de la lista.
+    b) Al final de la lista.
+  Finalmente, imprimir la lista.
+*/
+
+#include <iostream>
+
+int main () {
+
+  return 0;
+}
